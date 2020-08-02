8e2841dc90ae79e063c47733b15355343936729c
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-08-02 12:13:43 -0400

    added player win screen functionality

--- script.js
+++ script.js
@@ -1,57 +1,77 @@
-let turnColorIndicator = "white";
-let notTurnColorIndicator = "rgb(173, 173, 193)";
-let boardStatus = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-let winningPositions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
 
+
+
+
+let playerTurnColorIndicator = "white";
+let playerNotTurnColorIndicator = "rgb(173, 173, 193)";
+
+let boardStatus = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // 0 = empty | 1 = X | 2 = O
+let winningPositions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
 
 let playerOneObj; 
 let playerTwoObj; 
-let x = 0; 
+
+let filledPositionsCounter = 0; 
 
 
-if(document.querySelector("body").classList.contains("playerScreen"))
+loadAppropriateFunctionsJs();
+
+
+
+/**
+ * Determines which functions to load given the current web page
+ */
+
+function loadAppropriateFunctionsJs()
 {
-    x = 4; 
-    loadUserScreenPageJs();
-
-}else if (document.querySelector("body").classList.contains("playGameScreen"))
-{   
-    playerOneObj = JSON.parse(localStorage.getItem("playerOne"));
-    playerTwoObj = JSON.parse(localStorage.getItem("playerTwo"));
-
-    if(playerOneObj != null && playerTwoObj != null)
+    if(document.querySelector("body").classList.contains("playerScreen"))
     {
-        setStartingTurn();
-        loadPlayGamePageJs();
-        setGameBoardBlocks();
-    }else
-    {
-        // display a msg or redirect the user to login page 
+        loadUserScreenPageJs();
+
+    }else if(document.querySelector("body").classList.contains("playGameScreen"))
+    {   
+        playerOneObj = JSON.parse(localStorage.getItem("playerOne"));
+        playerTwoObj = JSON.parse(localStorage.getItem("playerTwo"));
+
+        if(playerOneObj != null && playerTwoObj != null)
+        {
+            loadPlayGamePageJs();
+        }else
+        {
+            // display a msg or redirect the user to login page 
+        }
     }
-    
 }
 
 
 
 function loadUserScreenPageJs()
 {
-    document.querySelector("#userOptions").addEventListener('submit', retrieveUserInfo);
+    document.querySelector("#userOptions").addEventListener('submit', retrieveUserInfo); 
     radioButtonSetUp();
 }
 
 function loadPlayGamePageJs()
 {
+    setStartingTurn();
     changeBoardPlayerNames();
+    setGameBoardBlocks();
+    setPlayerWonOptionBtn();
 
 }
 
 
+////////////////////////// player screen option section ////////////////////////
 
+/**
+ * Creates a player object
+ * @param {*} name  name given by user
+ * @param {*} symbol user selected symbol
+ */
 const PlayerFactory = (name, symbol) =>
 {
     return {name, symbol};
 }
-
 
 
 /**
@@ -70,7 +90,8 @@ function radioButtonSetUp()
 
 /**
  * 
- * Selects the opposite radio button (symbol) for the other player
+ * Selects the opposite radio button ( x / O symbol) for the other player
+ * insuring both players don't have the same symbol. 
  * 
  * @param {*} e 
  */
@@ -120,8 +141,13 @@ function checkRadioClicked(e)
 }
 
 
+
+
+
+
+
 /**
- * checks to make sure input is filled 
+ * Retrieves the user inputed information and saves it to be used on play game page 
  */
 function retrieveUserInfo()
 {
@@ -145,22 +171,34 @@ function retrieveUserInfo()
     playerOneObj = PlayerFactory(playerOneName, playerOneSymbol);
     playerTwoObj = PlayerFactory(playerTwoName, playerTwoSymbol);
 
-    localStorage.setItem('playerOne', JSON.stringify(playerOneObj));
+    localStorage.setItem('playerOne', JSON.stringify(playerOneObj)); 
     localStorage.setItem('playerTwo', JSON.stringify(playerTwoObj));
 }
 
 
 
+
+
+
+
+
+
+
 ///////////////////////////////// play game screen section /////////////////////////////////
 
+/**
+*   Sets player one to start game 
+*/
 function setStartingTurn()
 {
-
-    document.querySelector("#playerOneInfo").firstElementChild.style.color = turnColorIndicator;
-    document.querySelector("#playerTwoInfo").firstElementChild.style.color = notTurnColorIndicator;
+    document.querySelector("#playerOneInfo").firstElementChild.style.color = playerTurnColorIndicator;
+    document.querySelector("#playerTwoInfo").firstElementChild.style.color = playerNotTurnColorIndicator;
 }
 
 
+/**
+ *  Displays the names of the players on the game screen 
+ */
 function changeBoardPlayerNames()
 {
     document.querySelector("#playerOneName").textContent = playerOneObj.name;
@@ -171,6 +209,11 @@ function changeBoardPlayerNames()
 }
 
 
+
+
+/**
+ * Sets each block in the board to call the appropriate method once clicked 
+ */
 function setGameBoardBlocks()
 {
     let gameBlocks = Array.from(document.querySelectorAll(".boardPosition"));
@@ -181,6 +224,14 @@ function setGameBoardBlocks()
     }
 }
 
+
+
+
+/**
+ * Ads the correct symbol in the game board block and calls function to check for win 
+ * @param {*} e event
+ */
+
 function addSymbolInBlock(e)
 {
     let event = e || window.event;  
@@ -188,25 +239,37 @@ function addSymbolInBlock(e)
 
     if (target.textContent === "")
     {
-        if(document.querySelector("#playerOneInfo").firstElementChild.style.color === turnColorIndicator)
+        if(document.querySelector("#playerOneInfo").firstElementChild.style.color === playerTurnColorIndicator)
         {
             target.textContent = playerOneObj.symbol;
-            document.querySelector("#playerOneInfo").firstElementChild.style.color = notTurnColorIndicator; 
-            document.querySelector("#playerTwoInfo").firstElementChild.style.color = turnColorIndicator;
+            document.querySelector("#playerOneInfo").firstElementChild.style.color = playerNotTurnColorIndicator; 
+            document.querySelector("#playerTwoInfo").firstElementChild.style.color = playerTurnColorIndicator;
             addSymbolInArray(target.id, playerOneObj.symbol);
-
         }else
         {
             target.textContent = playerTwoObj.symbol;
-            document.querySelector("#playerOneInfo").firstElementChild.style.color = turnColorIndicator; 
-            document.querySelector("#playerTwoInfo").firstElementChild.style.color = notTurnColorIndicator;
+            document.querySelector("#playerOneInfo").firstElementChild.style.color = playerTurnColorIndicator; 
+            document.querySelector("#playerTwoInfo").firstElementChild.style.color = playerNotTurnColorIndicator;
             addSymbolInArray(target.id, playerTwoObj.symbol);
-
         }
+
+        checkWin();
     }
-    checkWin();
 }
 
+
+
+
+/**
+ *  Ads the symbol placed in a game board block to the internal representation of the game 
+ * 
+ *  empty block is represented by the number 0 
+ *  x is represented by the number 1 
+ *  o is represented by the number 2 
+ * 
+ * @param {*} blockId id of block where the symbol was placed 
+ * @param {*} symbol symbol to be placed 
+ */
 function addSymbolInArray(blockId, symbol)
 {   
     let numberSymbol = 0; 
@@ -233,25 +296,24 @@ function addSymbolInArray(blockId, symbol)
         "positionEight":  7, 
         "positionNine":   8, 
     };
+
     boardStatus[boardPositionTranslation[blockId]] = numberSymbol;
 }
 
-function resetGameBoard()
-{
 
-}
-
-
-
+/**
+ * Checks to see if the player won or if it is a draw 
+ */
 function checkWin()
 {
-    let currentSymbol = 0; 
-    const winingNumber = 3; 
+
+    let winningSymbol; 
     let won = false; 
+    
     let xSymbol = 1; 
     let oSymbol = 2; 
 
-    // O(1)
+    filledPositionsCounter++;
 
     for(let boardOriantation = 0; boardOriantation <  winningPositions.length; boardOriantation++)
     {
@@ -264,25 +326,99 @@ function checkWin()
             if(boardStatus[positionOne] == boardStatus[positionTwo] &&  boardStatus[positionTwo] == boardStatus[positionThree])
             {
                 won = true; 
+
+                if(boardStatus[positionOne] === xSymbol)
+                {
+                    winningSymbol = "X";
+                }else
+                {
+                    winningSymbol = "O";
+                }
             }
         }
     }
 
-
-    // Call the thing to stop the game 
-    console.log(won);
+    if(won)
+    {
+        playerWon(winningSymbol);
+        filledPositionsCounter = 0; 
+        
+    }else if (filledPositionsCounter === 9)
+    {
+        filledPositionsCounter = 0; 
+        tie();
+    }
 }
 
 
 
+/**
+ * Displays a message if a player won the game 
+ * @param {*} symbol winning player symbol 
+ */
+
+function playerWon(symbol)
+{
+    document.querySelector(".youWonContainer").style.display = "flex";
+
+    if(playerOneObj.symbol === symbol)
+    {
+
+         document.querySelector(".playerName").textContent = playerOneObj.name;
+    }else
+    {
+        document.querySelector(".playerName").textContent = playerTwoObj.name;
+    }
+
+}
 
 
 
+/**
+ * Displays a message if a tied occured 
+ */
+function tie()
+{
+    document.querySelector(".youWonContainer").style.display = "flex";
+    document.querySelector(".playerName").textContent = ""
+    document.querySelector(".winStatus").textContent = "Tie!"
+}
 
 
+/**
+ * Resets the game
+ */
+function resetGameBoard()
+{
+    let boardBlocks = Array.from(document.querySelectorAll(".boardPosition"));
 
 
+    // Reset board array internal representation 
+    for(let i = 0; i < boardStatus.length; i++)
+    {
+        boardStatus[i] = 0; // 0 empty block 
+    }
 
+
+    // reset board in browsers 
+    for(let i = 0; i < boardBlocks.length; i++)
+    {
+        boardBlocks[i].textContent = "";
+    }
+
+    document.querySelector(".youWonContainer").style.display = "none";
+    document.querySelector("#playerOneInfo").firstElementChild.style.color = playerTurnColorIndicator;
+    document.querySelector("#playerTwoInfo").firstElementChild.style.color = playerNotTurnColorIndicator;
+}
+
+
+/**
+ * sets the buttons for the player won screen
+ */
+function setPlayerWonOptionBtn()
+{
+    document.querySelector("#youWonRestartGameBtn").addEventListener("click", resetGameBoard);
+}
 
 
 
